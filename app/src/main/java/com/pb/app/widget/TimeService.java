4aa3025fbf99666e73069a25b8138aa77a882658
Max <max_yong163@163.com>
2022-01-29 15:42:22 +0800

    增加点击事件。

--- app/src/main/java/com/pb/app/widget/TimeService.java
+++ app/src/main/java/com/pb/app/widget/TimeService.java
@@ -7,6 +7,7 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
+import android.text.format.Time;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -23,12 +24,8 @@ public class TimeService extends Service {
     private Timer mTimer;
     private SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
     private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-    Intent btnIntent1 = new Intent();
-    Intent btnIntent2 = new Intent();
-    Intent btnIntent3 = new Intent();
-    PendingIntent pendingIntent1;
-    PendingIntent pendingIntent2;
-    PendingIntent pendingIntent3;
+    private SimpleDateFormat sdf2 = new SimpleDateFormat("ss");
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -39,14 +36,6 @@ public class TimeService extends Service {
         super.onCreate();
         Log.i("xmg", "TimeService onCreate");
 
-        // 定义一个Intent来发送按钮Action
-        btnIntent1.setAction("tag_action1");
-        btnIntent2.setAction("tag_action2");
-        btnIntent3.setAction("tag_action3");
-        // 用Intent实例化一个PendingIntent
-        pendingIntent1 = PendingIntent.getService(this, 0, btnIntent1, 0);
-        pendingIntent2 = PendingIntent.getService(this, 0, btnIntent2, 0);
-        pendingIntent3 = PendingIntent.getService(this, 0, btnIntent3, 0);
     }
 
     @SuppressLint("NewApi")
@@ -68,19 +57,93 @@ public class TimeService extends Service {
 //        Log.i("xmg", "updateView");
         String time = sdf.format(new Date());
         String date = sdf1.format(new Date());
+        String time2 = sdf2.format(new Date());
+//        Time time = new Time();
+//        time.setToNow();
+//        int hour = time.hour;
+//        int min = time.minute;
+//        int second = time.second;
+//        int year = time.year;
+//        int month = time.month + 1;
+//        int day = time.monthDay;
+
         RemoteViews rv = new RemoteViews(getPackageName(), R.layout.time_widget);
         rv.setTextViewText(R.id.tv_widget_time, time);
         rv.setTextViewText(R.id.tv_widget_date, date);
-
-        rv.setOnClickPendingIntent(R.id.tv_top_left_btn, pendingIntent1);
-        rv.setOnClickPendingIntent(R.id.tv_top_right_btn, pendingIntent2);
-        rv.setOnClickPendingIntent(R.id.tv_bottom_btn, pendingIntent3);
+        rv.setTextViewText(R.id.tv_widget_time2, time2);
 
         AppWidgetManager manager = AppWidgetManager.getInstance(getApplicationContext());
         ComponentName cn = new ComponentName(getApplicationContext(), TimeWidget.class);
         manager.updateAppWidget(cn, rv);
 
     }
+
+//    private void UpdateWidget(Context context) {
+//        //不用Calendar，Time对cpu负荷较小
+//        Time time = new Time();
+//        time.setToNow();
+//        int hour = time.hour;
+//        int min = time.minute;
+//        int second = time.second;
+//        int year = time.year;
+//        int month = time.month + 1;
+//        int day = time.monthDay;
+//        String strTime = String.format("%02d:%02d:%02d %04d-%02d-%02d", hour, min, second, year, month, day);
+//        RemoteViews updateView = new RemoteViews(context.getPackageName(),
+//                R.layout.time_widget_layout);
+//
+//        //时间图像更新
+//        String packageString = "org.owl.mythou";
+//        String timePic = "time";
+//        int hourHbit = hour / 10;
+//        updateView.setImageViewResource(R.id.hourHPic, getResources().getIdentifier(timePic + hourHbit, "drawable", packageString));
+//        int hourLbit = hour % 10;
+//        updateView.setImageViewResource(R.id.hourLPic, getResources().getIdentifier(timePic + hourLbit, "drawable", packageString));
+//        int minHbit = min / 10;
+//        updateView.setImageViewResource(R.id.MinuteHPic, getResources().getIdentifier(timePic + minHbit, "drawable", packageString));
+//        int minLbit = min % 10;
+//        updateView.setImageViewResource(R.id.MinuteLPic, getResources().getIdentifier(timePic + minLbit, "drawable", packageString));
+//
+//        //星期几
+//        updateView.setTextViewText(R.id.weekInfo, getWeekString(time.weekDay + 1));
+//
+//        //日期更新，根据日期，计算使用的图片
+//        String datePic = "date";
+//        int year1bit = year / 1000;
+//        updateView.setImageViewResource(R.id.Year1BitPic, getResources().getIdentifier(datePic + year1bit, "drawable", packageString));
+//        int year2bit = (year % 1000) / 100;
+//        updateView.setImageViewResource(R.id.Year2BitPic, getResources().getIdentifier(datePic + year2bit, "drawable", packageString));
+//        int year3bit = (year % 100) / 10;
+//        updateView.setImageViewResource(R.id.Year3BitPic, getResources().getIdentifier(datePic + year3bit, "drawable", packageString));
+//        int year4bit = year % 10;
+//        updateView.setImageViewResource(R.id.Year4BitPic, getResources().getIdentifier(datePic + year4bit, "drawable", packageString));
+//        //月
+//        int mouth1bit = month / 10;
+//        updateView.setImageViewResource(R.id.mouth1BitPic, getResources().getIdentifier(datePic + mouth1bit, "drawable", packageString));
+//        int mouth2bit = month % 10;
+//        updateView.setImageViewResource(R.id.mouth2BitPic, getResources().getIdentifier(datePic + mouth2bit, "drawable", packageString));
+//        //日
+//        int day1bit = day / 10;
+//        updateView.setImageViewResource(R.id.day1BitPic, getResources().getIdentifier(datePic + day1bit, "drawable", packageString));
+//        int day2bit = day % 10;
+//        updateView.setImageViewResource(R.id.day2BitPic, getResources().getIdentifier(datePic + day2bit, "drawable", packageString));
+//
+//        //点击widget，启动日历
+//        Intent launchIntent = new Intent();
+//        launchIntent.setComponent(new ComponentName("com.mythou.mycalendar",
+//                "com.mythou.mycalendar.calendarMainActivity"));
+//        launchIntent.setAction(Intent.ACTION_MAIN);
+//        launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+//        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+//                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+//        PendingIntent intentAction = PendingIntent.getActivity(context, 0,
+//                launchIntent, 0);
+//        updateView.setOnClickPendingIntent(R.id.SmallBase, intentAction);
+//        AppWidgetManager awg = AppWidgetManager.getInstance(context);
+//        awg.updateAppWidget(new ComponentName(context, TimeWidgetSmall.class),
+//                updateView);
+//    }
+
 
     @Override
     public void onDestroy() {
--- app/src/main/java/com/pb/app/widget/TimeWidget.java
+++ app/src/main/java/com/pb/app/widget/TimeWidget.java
@@ -1,39 +1,103 @@
 package com.pb.app.widget;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.text.format.Time;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
 
 /**
  * Implementation of App Widget functionality.
  */
 public class TimeWidget extends AppWidgetProvider {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("xmg", "TimeWidget  onReceive  action="+intent.getAction());
+
+        Uri data = intent.getData();
+        int resID = -1;
+        if(data != null) {
+            resID = Integer.parseInt(data.getSchemeSpecificPart());
+        }
+        /**通过远程对象设置文字*/
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.time_widget);
+        switch (resID) {
+            case R.id.tv_top_left_btn:
+                Toast.makeText(context, "1111", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_top_right_btn:
+                Toast.makeText(context, "2222", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_bottom_btn:
+                Toast.makeText(context, "3333", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        // 获得appwidget管理实例，用于管理appwidget以便进行更新操作
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        // 相当于获得所有本程序创建的appwidget
+        ComponentName componentName = new ComponentName(context, TimeWidget.class);
+        // 更新appwidget
+        appWidgetManager.updateAppWidget(componentName, remoteViews);
+
         super.onReceive(context, intent);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
         Log.i("xmg", "TimeWidget  onUpdate");
+
+        /**构造RemoteViews实例*/
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.time_widget);
+        //添加点击事件
+        remoteViews.setOnClickPendingIntent(R.id.tv_top_left_btn, getPendingIntent(context, R.id.tv_top_left_btn));
+        remoteViews.setOnClickPendingIntent(R.id.tv_top_right_btn, getPendingIntent(context, R.id.tv_top_right_btn));
+        remoteViews.setOnClickPendingIntent(R.id.tv_bottom_btn, getPendingIntent(context, R.id.tv_bottom_btn));
+        // 更新Appwidget
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+
+        Time time = new Time();
+        time.setToNow();
+        //使用Service更新时间
+        Intent intent = new Intent(context, TimeService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+        //使用Alarm定时更新界面数据
+        AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarm.setRepeating(AlarmManager.RTC, time.toMillis(true), 60*1000, pendingIntent);
+
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    private PendingIntent getPendingIntent(Context context, int resID){
+        Intent intent = new Intent();
+        //注意这个intent构造的是显式intent，直接将这个广播发送给MyAppWidgetProvider，使用Action的方式接收不到
+        intent.setClass(context, TimeWidget.class);
+        intent.setData(Uri.parse("hx:" + resID));
+        return PendingIntent.getBroadcast(context, 0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
     public void onEnabled(Context context) {
         Log.i("xmg", "TimeWidget  onEnabled");
-        context.startService(new Intent(context, TimeService.class));//开启服务
+//        context.startService(new Intent(context, TimeService.class));//开启服务
     }
 
 
     @Override
     public void onDisabled(Context context) {
         Log.i("xmg", "TimeWidget  onDisabled");
-        context.stopService(new Intent(context, TimeService.class));//stop服务
+//        context.stopService(new Intent(context, TimeService.class));//stop服务
     }
 }
